37c6c64b28958c8ec97a317314448b4c260cd662
FidèleGastro <gomes.raphael.31@gmail.com>
2018-11-04 17:38:28 +0100

    change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Sylaroga",
+    author: "Duplex gang",
+    description: "La contre-agence web"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
