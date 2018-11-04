2efeeaddc36c5ef67bf4be9a06e5125011933a47
FidèleGastro <gomes.raphael.31@gmail.com>
2018-11-04 17:33:56 +0100

    add article

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,17 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Sylaroga</h1>
+                <p>Pas de facture, pas de devis,
+                <br/>Telle est notre philosophie.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Présentation</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Les locaux</a></li>
+                <li><a href="javascript:;" >toto</a></li>
+                <li><a href="javascript:;" >toto</a></li>
             </ul>
         </nav>
     </header>
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,12 @@ import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 
+import raphael from '../images/raphael.jpg'
+import jerome from '../images/jerome.jpg'
+import alexandre from '../images/alexandre.jpg'
+//import baptiste from '../images/baptiste.jpg'
+import ambre from '../images/ambre.jpg'
+
 class Main extends React.Component {
   render() {
 
@@ -12,20 +18,39 @@ class Main extends React.Component {
 
     return (
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
+          <h2 className="major">Présentation</h2>
+          <h3 className="">Ambre Palanque</h3>
+          <span className="image main"><img src={ambre} alt="" /></span>
+          <p>L'intello.</p>
+          <p>Dev' sur son temps sobre</p>
+
+          <h3 className="">Jérome Fabre</h3>
+          <span className="image main"><img src={jerome} alt="" /></span>
+          <p>Le bolide.</p>
+          <p>Graphiste hors tours du Man</p>
+		  <h3 className="">Baptiste Duprat</h3>
           <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          <p>Lead plagiste.</p>
+          <p>Chef de projet digital, pour l'instant</p>
+
+			<h3 className="">Alexandre Techer</h3>
+          <span className="image main"><img src={alexandre} alt="" /></span>
+          <p>L'exilé.</p>
+          <p>Intégrateur pour malvoyant</p>
+
+		  <h3 className="">Raphaël Gomes</h3>
+          <span className="image main"><img src={raphael} alt="" /></span>
+          <p>L'altérophobe.</p>
+          <p>Stagiaire Sushi</p>
           {close}
         </article>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
           <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <h3>Le duplex</h3>
+          <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FLEDUPLEX.TOULOUSE%2Fvideos%2F1944802345604251%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
           {close}
         </article>
 
